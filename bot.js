9b6daafee8f52a5b8606a820bb163ee79e377847
adnangemthing <53823301+adnangemthing@users.noreply.github.com>
2019-08-12 17:53:31 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,3 +6,18 @@ client.on('ready', () => {
 });
 
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "$";
+client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' ');
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`);
+ message.delete();
+};                                                                                                                       
